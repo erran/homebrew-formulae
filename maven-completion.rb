37c54eb902ab8312f22136beed261c58d3e1960e
Erran Carey <me@errancarey.com>
2013-10-26 02:55:42 -0500

    Add vowels to maven to conform with the existing homebrew formulae

--- maven-completion.rb
+++ maven-completion.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class MvnCompletion < Formula
+class MavenCompletion < Formula
   homepage 'https://github.com/ipwnstuff/mvn-completion'
   url 'https://github.com/ipwnstuff/mvn-completion/archive/v1.0.0.tar.gz'
   sha1 '9672c4ad6d39bc8b1bf0458afb6c1dea138e99d2'
