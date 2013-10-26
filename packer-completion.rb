1d95d016e5b07a6aad1c341c00b4a30d78d2b83b
Erran Carey <me@errancarey.com>
2013-10-26 02:27:53 -0500

    Use the actual SHA1 not the changeset ID

--- packer-completion.rb
+++ packer-completion.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PackerCompletion < Formula
   homepage 'https://github.com/ipwnstuff/packer-completion'
   url 'https://github.com/ipwnstuff/packer-completion/archive/v1.0.0.tar.gz'
-  sha1 '7b2ea1bc36d2005d6bed4248869cbde8d0cff93a'
+  sha1 '270320e012eb3a7e6e5138b3b7d857954a31dcd0'
   head 'https://github.com/ipwnstuff/packer-completion.git'
 
   def install
